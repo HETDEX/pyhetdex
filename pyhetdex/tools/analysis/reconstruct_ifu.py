9c58f9f175435f74e703a6f3f07ccc468323ca49
Francesco Montesano <montefra@mpe.mpg.de>
2014-12-04 11:28:41 +0000

    2014-12-04  Francesco Montesano  <montefra@mpe.mpg.de>
    	* pyhetdex/tools/analysis: added
    	* pyhetdex/tools/analysis/seeing.py: added
    	* pyhetdex/tools/analysis/reconstruct_ifu.py: added
    
    
    
    git-svn-id: svn://luna.mpe.mpg.de/pyhetdex/trunk@6 79f4aad7-2104-455e-8097-9644d3262b72

---
+++ pyhetdex/tools/analysis/reconstruct_ifu.py
@@ -0,0 +1,352 @@
+"""
+Parse the ifu center file and the dither file
+"""
+from __future__ import print_function, absolute_import
+
+from collections import defaultdict
+import itertools as it
+import re
+
+from astropy.io import fits
+import numpy as np
+
+
+class ReconstructedIFU(object):
+    """
+    Reconstructed IFU head given the *ifu_center_file* and if any, the dither
+    file.
+    """
+
+    def __init__(self, ifu_center_file, dither_file=None, fextract=None):
+        """
+        Read and parse the file
+        Parameters
+        ----------
+        ifu_center_file: string
+            file containing the fiber number to fiber position mapping
+        dither_file: string
+            file containing the dither relative position. If not given, a singe
+            dither is assumed
+        fextract: None or list of fits files
+            if None the list of files is inferred from first or second column
+            of the *dither_file*;
+            if not None must have *ndither* x *nchannels* elements. The channel
+            name and dither number are extracted from the *CCDPOS* and the
+            *DITHER* header keywords
+        WARNING: *dither_file* and *fextract* cannot be *None* at the same time
+        WARNING: if *dither_file* is None, *fextract* must contain a number of
+        files equal  to the number of channels (2)
+        NOTE: should this distinction be moved to factory functions?
+        """
+        if dither_file is None and fextract is None:
+            msg = "dither_file and/or fextract must be provided"
+            raise ValueError(msg)
+
+        self._ifu_center_file = ifu_center_file
+
+        self._read_ifu(ifu_center_file)
+        self._read_dither(dither_file)
+        self._read_fextract(fextract)
+
+    def _read_ifu(self, ifu_center_file):
+        """
+        Read the ifu center file
+        Parameters
+        ----------
+        ifu_center_file: string
+            file containing the fiber number to fiber position mapping
+        """
+        with open(ifu_center_file, 'r') as f:
+            # get the fiber diameter and fiber separation
+            f = self._skip_comments(f)
+            line = f.readline()
+            self.fiber_d, self.fiber_sep = [float(i) for i in line.split()]
+            # get the number of fibers in the x and y direction
+            f = self._skip_comments(f)
+            line = f.readline()
+            self.nfibx, self.nfiby = [int(i) for i in line.split()]
+            # get from the rest of the file:
+            # the fiber position (second and third column)
+            # target unit spectrograph (L or R) (fourth column)
+            # target fiber within the spectrograph (fifth column)j
+            # relative throughput (sixth column)
+            f = self._skip_comments(f)
+            f = self._read_ifu_map(f)
+
+    def _skip_comments(self, f):
+        """
+        Skip commented lines and returns the file at the start of the first
+        line without any
+        Parameters
+        ----------
+        f: file object
+        output
+        ------
+        f: file object
+            moved to the next non comment line
+        """
+        pos = f.tell()
+        for l in f:
+            if l.startswith('#'):
+                pos += len(l)
+            else:
+                break
+        f.seek(pos)
+        return f
+
+    def _read_ifu_map(self, f):
+        """
+        Reads and store the remaining part of the file. Each row is expected to
+        be like:
+        ID        x ["]       y ["]    channel  fiber number        throughput
+                                                on spectrograph
+        0001      -22.88      -24.24   L (or R) 0001                1.000
+
+        The first column is ignored, the number of L or R is counted, the x, y
+        and fiber numbers are saved in lists stored in dictionaries with L or R
+        as keys. The throughput is used as check. Missing fibers are indicated
+        by negative fiber numbers or non integer values (like 'nan', '-', etc)
+        A ValueError is raised if a fiber number is positive and the throughput
+        zero
+        Parameters
+        ----------
+        f: file object
+        output
+        ------
+        f: file object
+            moved to the next non comment line
+        """
+        # start the dictionaries
+        self.xifu, self.yifu = defaultdict(list), defaultdict(list)
+        self.n_fibers = defaultdict(int)
+        self.fib_number = defaultdict(list)
+        self.throughput = defaultdict(list)
+
+        for line in f:
+            if line.startswith('#'):
+                continue
+            _x, _y, _channel, _fib_n, _t = line.split()[1:6]
+            # convert the fiber number to integer. If fails, means that the
+            # fiber is broken
+            try:
+                _fib_n = int(_fib_n)
+            except ValueError:
+                continue
+
+            if _fib_n > 0:
+                if float(_t) < 0.01:  # zero or less
+                    msg = 'In the fiber mapping file there is at least one'
+                    msg += ' fiber with positive fiber number and 0 throughput'
+                    msg += '. What should I do?'
+                    raise ValueError(msg)
+                else:
+                    self.n_fibers[_channel] += 1
+                    self.xifu[_channel].append(float(_x))
+                    self.yifu[_channel].append(float(_y))
+                    # As python indexing is 0 based, subtract 1 from the fiber
+                    # number
+                    self.fib_number[_channel].append(_fib_n - 1)
+                    self.throughput[_channel].append(float(_t))
+
+    def _read_dither(self, dither_file):
+        """
+        Read the relative dither position
+        Parameters
+        ----------
+        dither_file: string
+            file containing the dither relative position. If None a single
+            dither added
+        """
+        # delta x and y of the dithers
+        self.basename = {}
+        self.dx, self.dy = {}, {}
+        self.seeing, self.norm, self.airmass = {}, {}, {}
+
+        if dither_file is None:
+            self._no_dither()
+        else:
+            self._has_dither(dither_file)
+
+    def _has_dither(self, dither_file):
+        """
+        Read the relative dither position
+        Parameters
+        ----------
+        dither_file: string
+            file containing the dither relative position. If None a single
+            dither added
+        """
+        with open(dither_file, 'r') as f:
+            f = self._skip_comments(f)
+            for l in f:
+                try:
+                    _bn, _d, _x, _y, _seeing, _norm, _airmass = l.split()
+                    _d = list(set(re.findall(r'D\d', _d)))
+                    if len(_d) != 1:
+                        msg = "While extracting the dither number from the"
+                        msg += " basename in the dither file, {} matches to"
+                        msg += " 'D\\d' expression where found. I expected"
+                        msg += " one. What should I do?"
+                        raise ValueError(msg.format(len(_d)))
+                    self.basename[_d] = _bn
+                    self.dx[_d] = float(_x)
+                    self.dy[_d] = float(_y)
+                    self.seeing[_d] = float(_seeing)
+                    self.norm[_d] = float(_norm)
+                    self.airmass[_d] = float(_airmass)
+                except ValueError:
+                    # skip empty lines
+                    pass
+
+    def _no_dither(self):
+        "Fake a single dither"
+        _d = "D1"
+        self.basename[_d] = ""
+        self.dx[_d] = float(0)
+        self.dy[_d] = float(0)
+        self.seeing[_d] = float(0)
+        self.norm[_d] = float(0)
+        self.airmass[_d] = float(0)
+
+    def _read_fextract(self, fextract):
+        n_channels = len(self.xifu.keys())
+        n_dithers = len(self.dx.keys())
+        n_shots = n_channels * n_dithers
+
+        # the fiber extracted file names are saved into a dictionary with key
+        # like: L_D1
+        key = "{ch}_{d}"
+        dfextract = {}
+
+        if fextract is None:  # get the file names from the basename
+            fetemplate = "Fe{bn}_{ch}.fits"
+            for ch, (d, bn) in it.product(self.xifu.keys(), self.dx.item()):
+                fextract[key.format(ch=ch, d=d)] = fetemplate.format(ch=ch,
+                                                                     bn=bn)
+        else:  # the file name is provided
+            if len(fextract) != n_shots:
+                msg = "The number of fiber extracted files is different from"
+                msg += " {} ({} channels and {} dithers)"
+                raise ValueError(msg.format(n_shots, len(self.xifu.keys()),
+                                            len(self.dx.keys())))
+            for fe in fextract:
+                header = fits.getheader(fe)
+                ch = header['CCDPOS'].strip()
+                d = "D{0:d}".format(header['DITHER'])
+                dfextract[key.format(ch=ch, d=d)] = fe
+
+        # and now read and save the content of the fiber extracted files and
+        # reconstruct the full IFU image
+        self._reconstruct(dfextract)
+
+    def _reconstruct(self, dfextract):
+        """
+        Read the fiber extracted files and creates a set of three lists for x,
+        y and flux.
+        Parameters
+        ----------
+        dfextract: dictionary
+            name of the fiber extracted file
+        """
+        self.x, self.y = np.array([[], []], dtype=float)
+        self.flux, self.header = [], []
+        key = "{ch}_{d}"  # template for key of dfextract
+
+        for ch, d in it.product(self.xifu.keys(), self.dx.keys()):
+            # get the correct values of x and y
+            self.x = np.concatenate([self.x, self.xpos(ch, d)])
+            self.y = np.concatenate([self.y, self.ypos(ch, d)])
+
+            # read the fiber extracted file, order the fibers and save the
+            # necessary keys into self.h
+            fib_numbs = self.fib_number[ch]
+            with fits.open(dfextract[key.format(ch=ch, d=d)]) as hdu:
+                h = hdu[0].header
+                data = hdu[0].data
+                # if the number of fiber from the fiber extracted file is
+                # different from the number of fibers, something wrong
+                if data.shape[0] != len(fib_numbs):
+                    msg = "The number of rows in file '{}' does not agree"
+                    msg += "with the number of active fibers from file '{}'"
+                    raise IndexError(msg.format(hdu.filename(),
+                                                self._ifu_center_file))
+                self.flux.append(data[fib_numbs, :])  # reorder the fibers
+                # get the header keywords needed to get the index at a given
+                # wavelength
+                self.header.append({k: h.get(k) for k in ["CRVAL1", "CDELT1"]})
+
+    def xpos(self, channel, dither):
+        """
+        get the position for the x *dither* in *channel*
+        Parameters
+        ----------
+        channel: string
+            name of the channel [L, R]
+        dither: string
+            name of the dither [D1, D2, ..]
+        output
+        ------
+        xpos: ndarray
+            x position of the fibers for the given channel and dither
+        """
+        return np.array(self.xifu[channel]) + self.dx[dither]
+
+    def ypos(self, channel, dither):
+        """
+        get the position for the y *dither* in *channel*
+        Parameters
+        ----------
+        channel: string
+            name of the channel [L, R]
+        dither: string
+            name of the dither [D1, D2, ..]
+        output
+        ------
+        ypos: ndarray
+            y position of the fibers for the given channel and dither
+        """
+        return np.array(self.yifu[channel]) + self.dy[dither]
+
+    def reconstruct(self, wmin=None, wmax=None):
+        """
+        Returns the reconstructed IFU with the flux computed between
+        [wmin, wmax]
+        wmin, wmax: float
+            min and max wavelength to use. If *None*: use the min and/or max
+            from the file
+        output
+        ------
+        x, y: 1d arrays
+            x and y position of the fibers
+        flux: 1d array
+            flux of the fibers within *wmin* and *wmax*
+        """
+        _flux = []
+        for f, h in zip(self.flux, self.header):
+            _imin = self._indx_w(h, wmin)
+            _imax = self._indx_w(h, wmax)
+            _flux.append(f[:, _imin:_imax].sum(axis=1))
+
+        return self.x, self.y, np.concatenate(_flux)
+
+    def _indx_w(self, header, wavelength):
+        """
+        Using the "CRVAL1" and "CDELT1" keywords in *header* determine the
+        index of *wavelength*
+        Parameters
+        ----------
+        header: dictionary
+            dictionary containing the above keywords
+        wavelength: float
+            wavelength. If None, return None
+        output
+        ------
+        indx: int or None
+            index of *wavelength*
+        """
+        if wavelength is None:
+            return None
+        wmin = header["CRVAL1"]
+        deltaw = header["CDELT1"]
+        i = int(round((wavelength - wmin) / deltaw))
+        return i
---
+++ pyhetdex/tools/analysis/seeing.py
@@ -0,0 +1,311 @@
+"""
+Fits the point-like continuum detections with the Moffat distribution and
+returns alpha, beta and FWHM for the required detections.
+
+The Moffat distribution is defined in:
+http://en.wikipedia.org/wiki/Moffat_distribution
+"""
+
+from __future__ import print_function, absolute_import
+
+import itertools as it
+
+import numpy as np
+import scipy.optimize as spo
+
+
+# ==== The Moffat distribution ==== #
+def moffat(x, y, alpha, beta, ampl, xc, yc):
+    """
+    Implementation of the 2D Moffat distribution
+    Parameters
+    ----------
+    x, y: floats
+        where to evaluate the Moffat
+    alpha, beta: floats
+        parameters
+    xc, yc: floats
+        coordinates of the center
+    ampl: float
+        overall normalisation
+    output
+    ------
+    moff: float
+        Moffat(x, y)
+    """
+    norm = ampl * (beta - 1) * (np.pi * alpha**2) ** (-1)
+    return norm * (1 + ((x - xc)**2 + (y - yc)**2) * alpha**(-2)) ** (-beta)
+
+
+def get_fwhm(alpha, beta):
+    """
+    Compute the fwhm given alpha and beta
+    Parameters
+    ----------
+    alpha, beta: floats or nd arrays
+        parameters
+    output
+    ------
+    fwhm: float or nd array
+        full width half max
+    """
+    return 2. * alpha * np.sqrt(2**(1/beta) - 1)
+
+
+# ==== The integration ==== #
+class BaseSampler(object):
+    """
+    Sample each IFU with the given number of points. It caches the sampled
+    IFUs.
+    This is the base sampler and doesn't do the sampling. It provides a
+    function that returns the list of points for a given circle center and
+    radius. Anyway remember to call the init in any of the subclasses, as it
+    initialise the cache
+    """
+
+    def __init__(self):
+        """
+        Initialise the variable used.
+        _xs and _ys should be properly defined in the derived classes
+        """
+        self._cache = {}
+        self._xs, self._ys = None, None
+
+    def get_samples(self, x, y, r):
+        """
+        Get the sample point for the circle centered in (*x*, *y*) with radius
+        *r*
+        Parameters
+        ----------
+        x, y: floats
+            center of the circle
+        r: float
+            radius of the circle
+        output
+        ------
+        xs, ys: ndarray
+            points sampling the circle
+        """
+        # create an string id for the given circle. Discard any difference
+        # between the fourth digit
+        _id = "{0:.4f}_{1:.4f}_{2:.4f}".format(x, y, r)
+        try:
+            return self._cache[_id]
+        except KeyError:
+            # rescale to the correct radius and shift the sample points
+            _xs, _ys = self._xs * r, self._ys * r
+            _xs, _ys = _xs + x, _ys + y
+            # save the new values in the cache
+            self._cache[_id] = (_xs, _ys)
+            return _xs, _ys
+
+
+class RandomSampler(BaseSampler):
+    """
+    Randomly sample the circle
+    """
+
+    def __init__(self, n_points):
+        """
+        Creates about *n_points* random (x, y) coordinates that sample the unit
+        circle (x=0, y=0, r=0.5). It first creates a unite square (a=1) and
+        then removes the points outside the circle
+        Parameters
+        ----------
+        n_points: int
+            approximate number of points
+        """
+        super(RandomSampler, self).__init__()
+
+        _n_points = n_points * 4. / np.py
+        xy = np.random.random(size=[2, _n_points]) - 0.5
+        # find all the points in a circle of radius 0.5
+        in_circle = (xy**2).sum(axis=0) < 0.25
+        self._xs, self._ys = xy[:, in_circle]
+
+
+class GridSampler(BaseSampler):
+    """
+    Regular grid sample the circle
+    """
+
+    def __init__(self, n_points):
+        """
+        Creates about *n_points* (x, y) coordinates in a regular that sample
+        the unit circle (x=0, y=0, r=0.5). It first creates a unite square
+        (a=1) and then removes the points outside the circle.
+        The actual number of points is going to be almost:
+        round(sqrt(n_points * 4/pi)) ** 2
+        Parameters
+        ----------
+        n_points: int
+            approximate number of points
+        """
+        super(GridSampler, self).__init__()
+
+        _n_points = np.around(np.sqrt(n_points * 4 / np.pi))
+        x = np.linspace(-0.5, 0.5, num=_n_points)
+        x, y = np.meshgrid(x, x)
+        x, y = x.flatten(), y.flatten()
+        # find all the points in a circle of radius 0.5
+        in_circle = (x**2 + y**2) < 0.25
+        self._xs = x[in_circle]
+        self._ys = y[in_circle]
+
+
+# ==== Montecarlo like integration ==== #
+def montecarlo_2d(func, xy, area, params=()):
+    """
+    Compute the 2 dimensional integral of function *func* using a simple
+    montecarlo-like integration.
+    Parameters
+    ----------
+    func: callable
+        2-dimensional function to evaluate: f(x, y, *params)
+    xy: tuple or list of two arrays of size N or (2, N) array
+        x and y coordinates of the points to use to compute the integral
+    area: float
+        area sampled by *xy*
+    params: list
+        parameters to pass to *func*
+    output
+    ------
+    I: float
+        integral
+    sI: float
+        error on the integral
+    """
+    x, y = xy  # unpack
+    f_xy = func(x, y, *params)
+    # evaluate the integral and its error
+    I = area * f_xy.mean()
+    sI = area * f_xy.std(ddof=1) / np.sqrt(x.size)
+    return I, sI
+
+
+# ==== Residual ==== #
+def residuals(params, func, zmeas, x, y, r, sampler):
+    """
+    Given the parameters, computes the integral of *func* using the 2d
+    montecarlo integration and returns the difference with the measured *z*.
+    *x*, *y* and *r* are used to get the coordinated for the integration from
+    *sampler*
+    Parameters
+    ----------
+    params: list
+        parameters to pass to the Moffat
+    func: callable
+        2-dimensional function to evaluate: f(xsam, ysam, *params), where
+        *xsam* and *ysam* as extracted from *sampler*
+    zmeas: 1d array
+        measured value
+    x, y, r: 1d arrays
+        centers and radii of the circle to use as domain for the integral
+    sampler: BaseSampler instance
+        get the coordinates to use to estimate the integral
+    output
+    ------
+    residuals: 1d array
+        measurement minus model
+    """
+    residuals = np.empty_like(zmeas)
+
+    for i, x_, y_, a_, z_ in zip(it.count(), x, y, np.pi * r**2, zmeas):
+        xsam, ysam = sampler.get_samples(x_, y_, a_)
+        model = montecarlo_2d(func, [xsam, ysam], a_, params=params)
+        residuals[i] = z_ - model[0]
+
+    return residuals
+
+
+def seeing_minimizer(residual, func, init_params, ifuhead, detection,
+                     sampler, selection_radius=5, ifuhead_kwargs=None,
+                     lsq_kwargs=None):
+    """
+    Run the least square minimisation based on the *residual* of the function
+    *func*
+    Parameters
+    ----------
+    residual: callable
+        function that estimates the residual and that is passed to the least
+        square minimize. Signature:
+            residual(params, func, z, x, y, sampler)
+                params: parameter set
+                func: *func*
+                z: measured value (the integrate fiber flux from *ifuhead*)
+                x, y, r: center and radius of the circles where to do the
+                integration of *func* (centers and radii of the fibers from
+                    *focalplane*)
+                sampler: instance used to sample the circles (fibers)
+    func: callable
+        2-dimensional function to be integrated Signature:
+            func(xsam, ysam, *params), where
+                xsam, ysam: coordinates sampling the fibers. Retrieved from
+                    *sampler* and *x*, *y*, *r*
+    init_params: list
+        Initial parameters to pass to *func*
+    ifuhead: reconstruct_ify.ReconstructedIFU instance. Contains the
+        position, size and flux of the fibers in the focal plane
+    detection: instance with the 'icx', 'icy', 'fwhm_xy' fields
+        e.g.: quicklook:src.detection.PContDetection
+    selection_radius: float
+        all the fibers in *focalplane* within *selection_radius* x
+        *detection.fwhm_xy* are selected
+    sampler: BaseSampler instance
+        get the coordinates to use to estimate the integral
+    ifuhead_kwargs: dictionary
+        options to pass to ifuhead.reconstruct ('wmin', 'wmax')
+    lsq_kwargs: dictionary
+        options to pass to the leastsq function
+    output
+    ------
+    result: whatever leastsq returns
+        see
+        http://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.leastsq.html
+    """
+
+    # get the reconstructed IFU head
+    if ifuhead_kwargs is None:
+        ifuhead_kwargs = {}
+    x_fibs, y_fibs, flux_fibs = ifuhead.reconstruct(**ifuhead_kwargs)
+
+    # find the fibers to use for the fit
+    used_fibers = ((x_fibs - detection.icx)**2 + (y_fibs - detection.icy)**2)
+    used_fibers = used_fibers <= selection_radius * detection.fwhm_xy
+    x_fibs = x_fibs[used_fibers]
+    y_fibs = y_fibs[used_fibers]
+    flux_fibs = flux_fibs[used_fibers]
+
+    # do the fit
+    if lsq_kwargs is None:
+        lsq_kwargs = {}
+    else:  # if there is any "args" key drop it
+        lsq_kwargs.pop("args", None)
+    # standard least square
+    radii = np.ones_like(x_fibs) * ifuhead.fiber_d/2.
+    args = (func, flux_fibs, x_fibs, y_fibs, radii, sampler)
+    result = spo.leastsq(residual, init_params, args=args, **lsq_kwargs)
+
+    return result
+
+
+def main():
+    """
+    Mock main to show how to use it
+    """
+    import reconstruct_ifu as rci
+    # define sampler
+    sampler = GridSampler(10000)
+    # get the detection
+    detection = "get the detection from quicklook"
+    # set up the IFU head
+    ifuhead = rci.ReconstructedIFU("IFU_cent_file", dither_file="dither_file",
+                                   fextract=["Fejpes1.fits", "Fejpes2.fits",
+                                             "Fejpes3.fits", "Fejpes4.fits",
+                                             "Fejpes5.fits", "Fejpes1.fits"]
+
+    fit = seeing_minimizer(residuals, montecarlo_2d, [2, 2, 10, detection.icx,
+                                                      detection.icy], ifuhead,
+                           detection, sampler, ifuhead_kwargs={'wmin': 4000, 
+                                                               "wmax": 5000}, 
+                           sq_kwargs={'full_output': True, 'epsfcn': 1e-3})
