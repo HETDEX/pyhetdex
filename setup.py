4cadcfa5734fc6343c4650ec8406098e9bfc7086
Francesco Montesano <montefra@mpe.mpg.de>
2016-06-06 18:04:07 +0000

    bump version to 0.8.0-post
    git-svn-id: svn://luna.mpe.mpg.de/pyhetdex/trunk@175 79f4aad7-2104-455e-8097-9644d3262b72

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ entry_points = {'console_scripts':
 setup(
     # package description and version
     name="pyhetdex",
-    version="0.8.0",
+    version="0.8.0-post",
     author="HETDEX collaboration",
     author_email="montefra@mpe.mpg.de",
     description="Heterogeneous collection of HETDEX-related functionalities",
