4ab15b1e0334d0b12d0e7535b9dc9aa32b2d929a
Francesco Montesano <montefra@mpe.mpg.de>
2016-11-23 14:48:25 +0000

    2016-11-23 Francesco Montesano <montefra@mpe.mpg.de>
        * pyhetdex/coordinates/astrometry.py: parent argument parsers and PEP8 
        * pyhetdex/tools/read_catalogues.py: PEP8
        * tests/test_coordinates/test_astrometry.py: same
    
    
    
    git-svn-id: svn://luna.mpe.mpg.de/pyhetdex/trunk@240 79f4aad7-2104-455e-8097-9644d3262b72

--- pyhetdex/tools/read_catalogues.py
+++ pyhetdex/tools/read_catalogues.py
@@ -4,9 +4,8 @@ from astropy.table import Table
 
 
 def read_matched_line_detect(fn):
-    """ 
-    Read in line detect file matched to a simsrc input file
-         
+    """Read in line detect file matched to a simsrc input file
+
     Parameters
     ----------
     fn : str
@@ -18,19 +17,19 @@ def read_matched_line_detect(fn):
         the x, y coordinates from the file
     table : astropy.table.Table
         the rest of the table
-    """ 
-    table = Table.read(fn, format='ascii', names=('ID_in','xin','yin','l_rest','flux_in','zin',
-                                  'NR','ID','XS','YS','l','z','dataflux',
-                                  'modflux','fluxfrac','sigma','chi2','chi2s',
-                                  'chi2w','gammq','gammq_s','eqw', 'cont', 'separation'))
-    
+    """
+    table = Table.read(fn, format='ascii',
+                       names=('ID_in', 'xin', 'yin', 'l_rest', 'flux_in',
+                              'zin', 'NR', 'ID', 'XS', 'YS', 'l', 'z',
+                              'dataflux', 'modflux', 'fluxfrac', 'sigma',
+                              'chi2', 'chi2s', 'chi2w', 'gammq', 'gammq_s',
+                              'eqw',  'cont',  'separation'))
     return table['xin'], table['yin'], table
- 
+
 
 def read_simsrc_in(fn):
-    """ 
-    Read in a simsrc input file
-         
+    """Read in a simsrc input file
+
     Parameters
     ----------
     fn : str
@@ -42,41 +41,16 @@ def read_simsrc_in(fn):
         the x, y coordinates from the file
     table : astropy.table.Table
         the rest of the table
-    """ 
-    table = Table.read(fn, format='ascii', names=(
-                    'ID','xin','yin','l_rest','flux_in','zin'), comment='#')
-    
+    """
+    table = Table.read(fn, format='ascii', comment='#',
+                       names=('ID', 'xin', 'yin', 'l_rest', 'flux_in', 'zin'))
+
     return table['xin'], table['yin'], table
-   
 
 
 def read_line_detect(fn):
-    """ 
-    Read in a line file from detect
-         
-    Parameters
-    ----------
-    fn : str
-        the filename to read
+    """Read in a line file from detect
 
-    Returns
-    -------
-    x, y : array
-        the x, y coordinates from the file
-    table : astropy.table.Table
-        the rest of the table
-    """ 
-    table = Table.read(fn, format='ascii', names= (
-                    'NR','ID','XS','YS','l','z','dataflux','modflux','fluxfrac','sigma','chi2','chi2s',
-                    'chi2w','gammq','gammq_s','eqw', 'cont'), comment='#')
-    
-    return table['XS'], table['YS'], table
-  
-
-def read_cont_detect(fn):
-    """ 
-    Read in a cont file from detect
-         
     Parameters
     ----------
     fn : str
@@ -89,17 +63,41 @@ def read_cont_detect(fn):
     table : astropy.table.Table
         the rest of the table
     """
-    table = Table.read(fn, format='ascii', names= (
-                        'ID','icx','icy','<sigma>','fwhm_xy','a','b','pa','ir1',',ka',
-                        'kb','xmin','xmax','ymin','ymax','zmin','zmax'), comment='#')
+    table = Table.read(fn, format='ascii', comment='#',
+                       names=('NR', 'ID', 'XS', 'YS', 'l', 'z', 'dataflux',
+                              'modflux', 'fluxfrac', 'sigma', 'chi2', 'chi2s',
+                              'chi2w', 'gammq', 'gammq_s', 'eqw',  'cont'))
+
+    return table['XS'], table['YS'], table
+
+
+def read_cont_detect(fn):
+    """Read in a cont file from detect
+
+    Parameters
+    ----------
+    fn : str
+        the filename to read
+
+    Returns
+    -------
+    x, y : array
+        the x, y coordinates from the file
+    table : astropy.table.Table
+        the rest of the table
+    """
+    table = Table.read(fn, format='ascii', comment='#',
+                       names=('ID', 'icx', 'icy', '<sigma>', 'fwhm_xy', 'a',
+                              'b', 'pa', 'ir1', ', ka', 'kb',  'xmin', 'xmax',
+                              'ymin', 'ymax', 'zmin', 'zmax'))
 
     return table['icx'], table['icy'], table
 
+
 def read_daophot(fn):
-    """ 
-    Read in a DAOPHOT ALLSTAR file from
-    a run on a collapsed cube/apimage file
-         
+    """Read in a DAOPHOT ALLSTAR file from a run on a collapsed cube/apimage
+    file
+
     Parameters
     ----------
     fn : str
@@ -112,9 +110,9 @@ def read_daophot(fn):
     table : astropy.table.Table
         the rest of the table
     """
-    table = Table.read(fn, format='ascii', data_start=2, names=['ID', 'icx', 'icy', 'mag', 'mag_std', 'sky', 'niter', 'CHI', 'SHARP'])
+    table = Table.read(fn, format='ascii', data_start=2,
+                       names=['ID', 'icx', 'icy', 'mag', 'mag_std', 'sky',
+                              'niter', 'CHI', 'SHARP'])
 
     # transform to system where 0,0 is at center of IFU
     return table['icx'] - 24.5, table['icy'] - 24.5, table
-
-
