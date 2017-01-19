8ee08b53108df0ceee536c9c539f71dd97583541
Francesco Montesano <montefra@mpe.mpg.de>
2017-01-19 13:45:31 +0000

    2017-01-19 Francesco Montesano <montefra@mpe.mpg.de>
    	* doc/source/tools/six_ext.rst: update documentation; solves issue #988
        * pyhetdex/tools/six_ext.py: same
    
    
    
    git-svn-id: svn://luna.mpe.mpg.de/pyhetdex/trunk@255 79f4aad7-2104-455e-8097-9644d3262b72

--- doc/source/tools/six_ext.rst
+++ doc/source/tools/six_ext.rst
@@ -3,9 +3,5 @@
 :py:mod:`six_ext` -- Python 2 and 3 compatibility layer
 =======================================================
 
-.. automodule:: pyhetdex.tools.six_ext
-   :member-order: groupwise
-   :members:
-   :undoc-members:
-   :private-members:
 
+.. automodule:: pyhetdex.tools.six_ext
--- pyhetdex/tools/six_ext.py
+++ pyhetdex/tools/six_ext.py
@@ -1,5 +1,48 @@
 """Extensions to ``six`` python 2 and 3 compatibility layer.
 
+The following exceptions are more descriptive aliases to python 3 or python 2
+exceptions.
+
+.. list-table:: Exceptions
+    :header-rows: 1
+    :widths: 1 1 1 1
+
+    * - Name
+      - Python 3 name
+      - Python 2 name
+      - Description
+    * - :exc:`FileOpenError`
+      - :exc:`FileNotFoundError`
+      - :exc:`IOError`
+      - File not found
+    * - :exc:`SubprocessExeError`
+      - :exc:`FileNotFoundError`
+      - :exc:`OSError`
+      - :class:`subprocess.Popen` does not find the file to execute
+    * - :exc:`ConfigFileError`
+      - :exc:`FileNotFoundError`
+      - :exc:`OSError`
+      - Configuration file not found
+
+The module also provided some modules renamed as described in the `six
+documentation <https://pythonhosted.org/six/#module-six.moves>`_.
+
+.. list-table:: Renamed modules
+    :header-rows: 1
+
+    * - Name
+      - Python 3 name
+      - Python 2 name
+    * - mock
+      - unittest.mock
+      - mock
+
+Example of used of renamed modules
+
+>>> from pyhetdex.tools.six_ext import mock
+>>> # or if :mod:`~pyhetdex.tools.six_ext` has already been imported somewhere
+>>> # in the code
+>>> from six.moves import mock  # doctest: +SKIP
 """
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
@@ -9,16 +52,11 @@ import six
 # === Exception definitions
 if six.PY3:
     FileOpenError = FileNotFoundError
-    """File not found"""
     SubprocessExeError = FileNotFoundError
-    """:class:`subprocess.Popen` does not find the file to execute"""
     ConfigFileError = FileNotFoundError
-    """Configuration file not found"""
 else:
     FileOpenError = IOError
-    """File not found"""
     SubprocessExeError = OSError
-    """:class:`subprocess.Popen` does not find the file to execute"""
 
     class ConfigFileError(OSError):
         """Configuration file not found"""
@@ -26,13 +64,4 @@ else:
 
 # === module definitions
 mock = six.MovedModule("mock", "mock", "unittest.mock")
-"""This is to be imported as a standard module.
-
-Examples
---------
->>> from pyhetdex.tools.six_ext import mock
->>> # or if :mod:`~pyhetdex.tools.six_ext` has already been imported somewhere
->>> # in the code
->>> from six.moves import mock
-"""
 six.add_move(mock)  # move to six.moves
